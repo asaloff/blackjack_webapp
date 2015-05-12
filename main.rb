c6761d87ec0cd00daac0a09856b56f633c68a40c
Aaron Saloff <aaronsaloff@gmail.com>
2015-05-11 22:49:13 -0400

    day 1

--- main.rb
+++ main.rb
@@ -1,15 +1,25 @@
 require 'rubygems'
 require 'sinatra'
 
-use Rack::Session::Cookie, :key => 'rack.session',
-                           :path => '/',
-                           :secret => 'your_secret'
+set :sessions, true
 
-
-get '/form' do
-  erb :form
+get '/' do
+  if session[:player_name]
+    redirect "/game"
+  else
+    redirect "/get_username"
+  end
 end
 
-get '/nested_template' do
-  erb :"/nest/nested"
+get '/get_username' do
+  erb :player_name_form
+end
+
+post '/set_player_name' do
+  session[:player_name] = params['player_name']
+  redirect '/game'
+end
+
+get '/game' do
+  "Hello" + session[:player_name].to_s
 end
