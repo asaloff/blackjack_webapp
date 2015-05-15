77f461d20305b2cbcbf74360e0dc89d442c206e6
Aaron Saloff <aaronsaloff@gmail.com>
2015-05-15 16:08:12 -0400

    Added betting functionality and fixed various bugs

--- main.rb
+++ main.rb
@@ -51,11 +51,16 @@ helpers do
     "<img class='card_resize' src='/images/cards/#{suit}_#{value}.jpg'>"
   end
 
+  def pay_up
+    2.times { session[:player_wallet] += session[:pot] }
+  end
+
   def blackjack(player)
     if player == 'dealer'
       @error = "Dealer has Blackjack...Bummer."
     else
       @success = "#{session[:player_name]} has Blackjack!!"
+      pay_up
     end
     @show_hit_stay_buttons = false
     @replay = true
@@ -64,6 +69,7 @@ helpers do
   def busted(player)
     if player == 'dealer'
       @success= "Dealer busts!! #{session[:player_name]} wins!!"
+      pay_up
     else
       @error = "#{session[:player_name]} busts! Maybe next time."
     end
@@ -80,17 +86,26 @@ helpers do
 
     if player_total > dealer_total
       @success = "#{session[:player_name]}'s cards are better! #{session[:player_name]} wins!!"
+      pay_up
     elsif player_total < dealer_total
       @error = "Dealer's cards are better. #{session[:player_name]} loses."
     elsif player_total == dealer_total
       @neutral = "#{session[:player_name]}'s and the Dealer's cards are the same. It's a tie."
+      session[:player_wallet] += session[:pot]
     end
     @replay = true
   end
 
-  def display_play_again_buttons
-    "<p class='play-again'><strong>Would you like to play again?</strong> 
-    <a class='btn btn-primary' href='/game'>Yes</a> <a class='btn' href='/end_game'>No</a></p>"
+  def display_play_again_buttons  
+    "<h3>Would you like to play again?</h3>
+      <div class='seg'>
+        <form id='yes_button' action='/game/play_again' method='post'>
+          <input type='submit' value='Yes' class='btn btn-success'>
+        </form>
+        <form id='no_button' action='/game/leave' method='post'>
+          <input type='submit' value='No' class='btn btn-danger'>
+        </form>
+      </div>"
   end
 
 end
@@ -102,29 +117,66 @@ before do
 end
 
 get '/' do
+
   if session[:player_name]
-    redirect "/game"
+    redirect "/make_bet"
   else
     redirect "/get_username"
   end
 end
 
 get '/get_username' do
+
   erb :player_name_form
 end
 
 post '/set_player_name' do
 
-  if params['player_name'].empty?
+  if params[:player_name].empty?
     @error = 'Required feild is empty.'
+    halt erb :player_name_form
+  elsif params[:player_name] =~ /[^a-zA-Z ]/
+    @error = 'Only letters please...try again'
     halt erb :player_name_form
   end
 
-  session[:player_name] = params['player_name'].capitalize
+  session[:player_name] = params[:player_name]
+  session[:player_name] = session[:player_name].split(/ |\_/).map(&:capitalize).join(" ")
+  session[:player_wallet] = 500
+  redirect '/make_bet'
+end
+
+get '/make_bet' do
+
+  if session[:player_wallet] == 0
+    redirect '/out_of_money'
+  end
+  erb :make_bet
+end
+
+get '/out_of_money' do
+  erb :out_of_money
+end
+
+post '/set_bet_amount' do
+
+  if params[:bet] =~ /[^\d]/
+    @error = "Whole Numbers Only!"
+    halt erb :make_bet
+  elsif params[:bet].to_i > session[:player_wallet]
+    @error = "You can't bet what you don't have!"
+    halt erb :make_bet
+  elsif params[:bet].empty?
+    @error = "You can't begin without a bet"
+    halt erb :make_bet
+  end
+  session[:pot] = params[:bet].to_i
+  session[:player_wallet] -= session[:pot]
   redirect '/game'
 end
 
 get '/game' do
+
   cards = %W[A 2 3 4 5 6 7 8 9 10 J Q K]
   suits = %w[H S C D]
   session[:deck] = suits.product(cards).shuffle!
@@ -143,6 +195,7 @@ get '/game' do
 end
 
 post '/game/player/hit' do
+
   session[:player_cards] << session[:deck].shift
   total = calculate_total(session[:player_cards])
   if total > BLACKJACK_AMOUNT
@@ -154,11 +207,13 @@ post '/game/player/hit' do
 end
 
 post '/game/player/stay' do
+
   @show_hit_stay_buttons = false
   redirect '/game/dealer'
 end
 
 get '/game/dealer' do
+
   @show_hit_stay_buttons = false
   @player_turn = false
 
@@ -178,17 +233,28 @@ get '/game/dealer' do
 end
 
 post '/game/dealer/hit' do
+
   session[:dealer_cards] << session[:deck].shift
   redirect '/game/dealer'
   erb :game
 end
 
 get '/game/compare_cards' do
+
   compare_cards
   erb :game
 end 
 
+post '/game/play_again' do
+  redirect '/make_bet'
+end
+
+post '/game/leave' do
+  redirect 'end_game'
+end
+
 get '/end_game' do
+
   erb :end_game
 end
 
